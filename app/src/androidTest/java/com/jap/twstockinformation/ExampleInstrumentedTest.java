de39fbecf7b8020d42c398d3032cf6c7fdf3a3da
FrankFan <frank.fan@timecitytech.com>
2020-12-03 18:27:15 +0800

    no and name first version

--- app/src/androidTest/java/com/jap/twstockinformation/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jap/twstockinformation/ExampleInstrumentedTest.java
@@ -1,12 +1,17 @@
 package com.jap.twstockinformation;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -18,10 +23,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("app");
     }
-    public native String GetAllStockNumber();
-
+    public native String getresult(String CACERT);
+    public native ArrayList<String> getGerritChanges(String CACERT);
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -34,7 +39,10 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.jap.twstockinformation", appContext.getPackageName());
-        System.out.println(GetAllStockNumber());
+        String CACERT_PATH = appContext.getFilesDir() + File.separator + "cacert.pem";
+        Log.e("CACERT_PATH",CACERT_PATH);
+        System.out.println(getGerritChanges(CACERT_PATH));
+
     }
 
 }
--- app/src/main/java/com/jap/twstockinformation/MainActivity.java
+++ app/src/main/java/com/jap/twstockinformation/MainActivity.java
@@ -1,15 +1,20 @@
 package com.jap.twstockinformation;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("app");
     }
 
     @Override
@@ -18,15 +23,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = findViewById(R.id.sample_text);
+        /**
+         * download the cacert.pem from openssl website
+         * copy the ssl cacert.pem for libcurl from assets
+         * */
+        String CACERT_PATH = getFilesDir() + File.separator + "cacert.pem";
+        try {
+            InputStream in = getApplicationContext().getAssets().open("cacert.pem");
+            File outFile = new File(getFilesDir() + File.separator, "cacert.pem");
+            OutputStream out = new FileOutputStream(outFile);
+            byte[] buffer = new byte[1024*5];
+            int read;
+            while((read = in.read(buffer)) != -1){
+                out.write(buffer, 0, read);
+            }
+            out.flush();
+            out.close();
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String result = getresult(CACERT_PATH);
+        Log.e("MainActivity",result);
+//        for (int i=0; i <result.length;i++){
+//            Log.e("MainActivity",result[i]);
+//        }
+
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
-    public native String GetAllStockNumber();
-
+//    public native String stringFromJNI();
+    public native String[] getGerritChanges(String A);
+    public native String getresult(String A);
 }
